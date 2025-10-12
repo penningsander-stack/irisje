603b6b9663ae86d4cc266e7ca9c7c470f30951da
penningsander-stack <penningsander@gmail.com>
2025-10-13 00:03:59 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://irisje.onrender.com';
 
 function calcAverage(reviews) {
   if (!reviews || reviews.length === 0) return 0;
