065e2916be5a4eb0518c09a9d95487a607302bd8
penningsander-stack <penningsander@gmail.com>
2025-10-14 00:24:35 +0200

    Update review.js

--- backend/models/review.js
+++ backend/models/review.js
@@ -6,10 +6,20 @@ const reviewSchema = new mongoose.Schema({
     ref: 'Company',
     required: true
   },
-  author: { type: String, required: true },
-  rating: { type: Number, required: true },
-  comment: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  author: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Review', reviewSchema);
