4a41acd270d136a1c971df32debecb03648928a1
penningsander-stack <penningsander@gmail.com>
2025-10-13 22:11:28 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,23 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 require('dotenv').config();
 
-const companiesRoute = require('./routes/companies');
-const reviewsRoute = require('./routes/reviews');
+const companyRoutes = require('./routes/companies');
+const reviewRoutes = require('./routes/reviews');
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Database
 connectDB();
 
-app.use('/api/companies', companiesRoute);
-app.use('/api/reviews', reviewsRoute);
+// Routes
+app.use('/api/companies', companyRoutes);
+app.use('/api/reviews', reviewRoutes);
 
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
