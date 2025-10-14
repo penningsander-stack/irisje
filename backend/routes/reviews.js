6d7dd0b8152c79ea6dfaf562cc216f5fe14636fa
penningsander-stack <penningsander@gmail.com>
2025-10-14 17:38:30 +0200

    Update reviews.js

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Review = require('../models/Review');
+const Review = require('../models/review');
 
 // GET reviews voor bedrijf
 router.get('/:companyId', async (req, res) => {
