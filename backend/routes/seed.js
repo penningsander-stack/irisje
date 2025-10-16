669c6aecf95c5be6865776b63ffb0554973a1a24
penningsander-stack <penningsander@gmail.com>
2025-10-16 11:58:08 +0200

    Create seed.js

---
+++ backend/routes/seed.js
@@ -0,0 +1,22 @@
+Bedrijfsinformatie
+E-mail:
+
+📊 Statistieken
+Totaal
+0
+
+Geaccepteerd
+0
+
+Afgewezen
+0
+
+Opgevolgd
+0
+
+📬 Aanvragen
+Naam of e-mail
+
+Alle
+Filteren
+Naam	E-mail	Bericht	Status	Actie
