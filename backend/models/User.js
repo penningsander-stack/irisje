e9766d389070ff37789c594fdf2f13a849d29bba
penningsander-stack <penningsander@gmail.com>
2025-10-19 00:35:40 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,14 @@
 // backend/models/User.js
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // BELANGRIJK
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true }, // <<< hier klopt het nu
+  role: { type: String, default: "company" },
+  isActive: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model("User", userSchema);
