0710ccf070e122806199e8529613f3507a07f780
penningsander-stack <penningsander@gmail.com>
2025-10-18 23:44:02 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,13 @@
 // backend/models/User.js
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true, lowercase: true, index: true },
-    passwordHash: { type: String, required: true },
-    role: { type: String, enum: ['company', 'admin'], default: 'company' },
-    company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' }, // optioneel, kan later gebruikt worden
-    isActive: { type: Boolean, default: true }
+    name: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // BELANGRIJK
   },
   { timestamps: true }
 );
 
-userSchema.methods.comparePassword = async function (plain) {
-  return bcrypt.compare(plain, this.passwordHash);
-};
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
