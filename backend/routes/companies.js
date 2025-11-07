2c1ce1f3612059f14990cb38cf710323893e2c74
penningsander-stack <penningsander@gmail.com>
2025-11-08 00:43:21 +0100

    Update companies.js

--- backend/routes/companies.js
+++ backend/routes/companies.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const Company = require("../models/Company");
-const Review = require("../models/Review");
+const Review = require("../models/review");
 
 // ✅ Alle bedrijven ophalen
 router.get("/", async (req, res) => {
