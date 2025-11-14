fee6742dad6ddc125d85279e6c6b6eeb454628a5
penningsander-stack <penningsander@gmail.com>
2025-11-14 20:40:07 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./frontend/**/*.{html,js}",   // zoekt in alle HTML en JS binnen frontend/
-    "../frontend/**/*.{html,js}",  // zoekt ook als build in /frontend zelf draait
-    "./*.html"                     // zoekt naar losse HTML-bestanden in root
+    "./frontend/**/*.html",
+    "./frontend/**/*.js"
   ],
   theme: {
     extend: {},
