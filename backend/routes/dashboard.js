5a089d353966415631248702c91b50c4d64ee0d9
penningsander-stack <penningsander@gmail.com>
2025-11-14 18:55:43 +0100

    Update dashboard.js

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,7 +1,7 @@
 // backend/routes/dashboard.js
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const Request = require("../models/equest");
+const Request = require("../models/Request");
 
 const router = express.Router();
 
