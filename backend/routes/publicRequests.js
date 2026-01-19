a7d06bd6c3e12de52a3c1c1a7c78beae5dae3856
penningsander-stack <penningsander@gmail.com>
2026-01-19 11:18:28 +0100

    Update publicRequests.js

--- backend/routes/publicRequests.js
+++ backend/routes/publicRequests.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const router = express.Router();
 
-const Request = require("../models/Request");
-const Company = require("../models/Company");
+const Request = require("../models/request");
+const Company = require("../models/company");
 
 /*
   GET /api/publicRequests/:id
